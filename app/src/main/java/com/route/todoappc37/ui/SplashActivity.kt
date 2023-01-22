fefa50dd1b8ea1cc13bedf93a24261ec6da83ef8
Ahmed <an0088039@gmail.com>
2023-01-22 20:14:08 +0200

    Added splash screen

---
+++ app/src/main/java/com/route/todoappc37/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.route.todoappc37.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.route.todoappc37.R
+import com.route.todoappc37.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startHomeScreen()
+                              }, 2000)
+    }
+    fun startHomeScreen(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/route/todoappc37/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.route.todoappc37.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.todoappc37.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
