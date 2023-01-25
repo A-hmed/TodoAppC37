3b6896186387910a549268f72fd2a0c7b15e0a66
Ahmed <an0088039@gmail.com>
2023-01-25 19:42:41 +0200

    Finished add todo bottom sheet

--- app/src/main/java/com/route/todoappc37/ui/home/HomeActivity.kt
+++ app/src/main/java/com/route/todoappc37/ui/home/HomeActivity.kt
@@ -1,12 +1,46 @@
 package com.route.todoappc37.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
 import com.route.todoappc37.R
+import com.route.todoappc37.ui.home.fragments.AddBottomSheetFragment
+import com.route.todoappc37.ui.home.fragments.ListFragment
+import com.route.todoappc37.ui.home.fragments.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
+    lateinit var fab: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottomNav = findViewById(R.id.bottomNavigation)
+        fab = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val addBottomSheet = AddBottomSheetFragment()
+            addBottomSheet.show(supportFragmentManager, "")
+        }
+        pushFragment(ListFragment())
+        bottomNav.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                if (item.itemId == R.id.listItem) {
+                    pushFragment(ListFragment())
+                } else {
+                    pushFragment(SettingsFragment())
+                }
+                return true
+            }
+        })
+    }
+
+    fun pushFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/route/todoappc37/ui/home/fragments/AddBottomSheetFragment.kt
@@ -0,0 +1,86 @@
+package com.route.todoappc37.ui.home.fragments
+
+import android.app.DatePickerDialog
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
+import com.route.todoappc37.R
+import java.util.*
+
+class AddBottomSheetFragment: BottomSheetDialogFragment() {
+    lateinit var selectDateTv:TextView
+    var selectedDate: Calendar= Calendar.getInstance()
+    lateinit var titleTextInput: TextInputLayout
+    lateinit  var descriptionTextInput: TextInputLayout
+    lateinit var addTodoButton: Button
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_todo, container,false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews(view)
+        initListeners()
+    }
+    fun initViews(view: View){
+        selectDateTv = view.findViewById(R.id.selectedDateTv)
+        titleTextInput = view.findViewById(R.id.titleTextInputLayout)
+        descriptionTextInput = view.findViewById(R.id.descriptionTextInput)
+        addTodoButton = view.findViewById(R.id.addTodoButton)
+        updateDateTextView()
+    }
+    fun initListeners(){
+        selectDateTv.setOnClickListener {
+            val dialog = DatePickerDialog(requireContext(),
+                { p0, year, month, day ->
+                    selectedDate.set(year, month, day)
+                    updateDateTextView()
+                }, selectedDate.get(Calendar.YEAR),
+                selectedDate.get(Calendar.MONTH),
+                selectedDate.get(Calendar.DAY_OF_MONTH))
+
+            dialog.show()
+        }
+
+        addTodoButton.setOnClickListener {
+              if(! validate()) return@setOnClickListener
+            //todo: we should add todo to database
+        }
+
+    }
+    fun validate(): Boolean{
+        var isValid = true
+        if(titleTextInput.editText!!.text.isEmpty()){
+            titleTextInput.error = "Please write todo title"
+            isValid = false
+        }else {
+            titleTextInput.error = null
+        }
+
+        if(descriptionTextInput.editText!!.text.isEmpty()){
+            descriptionTextInput.error = "Please write todo description"
+            isValid = false
+        }else {
+          descriptionTextInput.error = null
+        }
+        return isValid
+    }
+    fun updateDateTextView(){
+        selectDateTv.text = "${selectedDate.get(Calendar.DAY_OF_MONTH)}/ ${selectedDate.get(Calendar.MONTH)+1} / ${selectedDate.get(Calendar.YEAR)}"
+
+    }
+}
